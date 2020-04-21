5435f7f73b478a862a669454d2a6dadb7e161629
kevin aldian <kevin19aldian98@gmail.com>
2020-04-22 01:29:04 +0700

    Remove Unnecessary Comments

--- src/Encoder.ino
+++ src/Encoder.ino
@@ -34,33 +34,3 @@ void getPosition()
 
 
 
-//void updatePosition(){
-//  interv = millis() - last_time;
-//  if (interv > 10){
-//    for (int i = 0; i < 3; i++)
-//    {
-//      delta_count[i] = rot_count[i] - last_count[i];
-//      last_count[i] = rot_count[i];
-//    }   
-//    
-//    theta += -(delta_count[0] + delta_count[1] + delta_count[2]) * DIST_PER_PULSE / (3 * LENGTH);
-//    if (theta > 2 * PI) theta -= 2 * PI;
-//    else if(theta < 0) theta += 2 * PI;
-//    cos_theta = cos(theta);
-//    sin_theta = sin(theta);
-//
-//    x_count -= (sqrt3 * sin_theta * (delta_count[0] - delta_count[1]) + cos_theta * (delta_count[0] + delta_count[1] - 2 * delta_count[2])) / 3;
-//    y_count -= (-sqrt3 * cos_theta * (delta_count[0] - delta_count[1]) + sin_theta * (delta_count[0] + delta_count[1] - 2 * delta_count[2])) / 3;
-//
-//    posx = (int16_t)(x_count * DIST_PER_PULSE);
-//    posy = (int16_t)(y_count * DIST_PER_PULSE);
-//    orient = (int16_t)(theta * TO_DEG);
-//    if(orient == 360) orient = 0;
-//    else if(orient > 360) orient -= 360;
-//    else if(orient < 0) orient += 360;
-//
-//    last_time = millis();
-//  }
-//}
-
-
